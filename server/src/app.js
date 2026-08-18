ca5df946bffc26ac99f78af8af626c6604db6713
Anujgiri Goswami <anujgoswami0212@gmail.com>
2026-08-18 12:01:22 +0530

    Database setup for BuildBridge

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+// Connect to MongoDB
+connectDB();
 
-app.get('/', (req, res) => {
-    res.json({message: "Backened is running successfully"});
+app.get("/", (req, res) => {
+    res.send("BuildBridge Server is running!");
 });
 
 app.listen(PORT, () => {
-    console.log('Server executing live on http://localhost:5000');
+    console.log(`Server running on port ${PORT}`);
 });
