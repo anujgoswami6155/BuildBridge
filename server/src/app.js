f9f8011f651b09cc6216c0e11ce5ed25d9add9ea
Anujgiri Goswami <anujgoswami0212@gmail.com>
2026-08-23 19:05:27 +0530

    Add User model and backend comments

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 
+// Create an Express Application   
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -11,10 +12,13 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+// Register route
+//If a request comes to "/", execute this function
 app.get("/", (req, res) => {
     res.send("BuildBridge Server is running!");
 });
 
+// Starts the server and tells it to listen to the incoming requests
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ server/src/models/User.models.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+// Create a Schema for the model
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true,
+        minlength : 3,
+        maxlength : 50,
+    },
+
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+
+    passwordHash : {
+        type: String,
+        required: true,
+    },
+
+    bio : {
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 500,
+    },
+
+    skills : {
+        type: [String],
+        required: false,
+    },
+
+    github : {
+        type: String,
+        required: false,
+        trim: true,
+    },
+
+    linkedIn: {
+        type: String,
+        required: false,
+        trim: true,
+    }
+}, {
+    // To keep track of the createdAt and UpdatedAt
+    timestamp: true
+});
+
+
+const User = mongoose.model('User', userSchema);
+export default User;
